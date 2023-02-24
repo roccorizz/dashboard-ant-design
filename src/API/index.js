2f6cfc8445880b51bbc6e29196bdfc5767af5947
roccorizz <sknahid68@gmail.com>
2023-02-25 01:30:51 +0600

    notification added

--- src/API/index.js
+++ src/API/index.js
@@ -17,3 +17,8 @@ export const getCustomers = () => {
     return fetch('https://dummyjson.com/users')
         .then(res => res.json())
 }
+export const getComments = () => {
+    return fetch('https://dummyjson.com/comments')
+        .then(res => res.json())
+
+}
--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,22 +1,51 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BellFilled, HomeOutlined, MailOutlined } from '@ant-design/icons';
-import { Badge, Space } from 'antd';
+import { Badge, Drawer, List, Space, Typography } from 'antd';
+import { getComments, getOrders } from '../../API';
 
 function AppHeader() {
+    const [comments, setComments] = useState([])
+    const [orders, setOrders] = useState([]);
+    const [commentsOpen, setCommentsOpen] = useState(false);
+    const [notificationsOpen, setNotificationsOpen] = useState(false)
+
+    useEffect(() => {
+        getComments().then((res) => {
+            setComments(res.comments)
+        })
+        getOrders().then((res) => {
+            setOrders(res.products)
+        })
+    }, [])
+
     return (
         <div className='AppHeader'>
             <HomeOutlined style={{ fontSize: 24 }} />
             <Space>
-                <Badge count={10}>
+                <Badge count={comments.length} dot>
 
-                    <MailOutlined style={{ fontSize: 24 }} />
+                    <MailOutlined onClick={() => {
+                        setCommentsOpen(true)
+                    }} style={{ fontSize: 24 }} />
                 </Badge>
-                <Badge count={22}>
+                <Badge count={orders.length}>
 
-                    <BellFilled style={{ fontSize: 24 }} />
+                    <BellFilled onClick={() => {
+                        setNotificationsOpen(true)
+                    }} style={{ fontSize: 24 }} />
                 </Badge>
             </Space>
+            <Drawer title="Comments" open={commentsOpen} onClose={() => { setCommentsOpen(false) }} maskClosable>
+                <List dataSource={comments} renderItem={(item) => {
+                    return <List.Item>{item.body}</List.Item>
+                }}></List>
+            </Drawer>
+            <Drawer title="Notifications" open={notificationsOpen} onClose={() => { setNotificationsOpen(false) }} maskClosable>
+                <List dataSource={orders} renderItem={(item) => {
+                    return <List.Item><Typography.Text strong>{item.title}</Typography.Text>{" "} has been ordered.</List.Item>
+                }}></List>
+            </Drawer>
         </div>
     )
 }
